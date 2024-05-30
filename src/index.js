95af43a9cac23cb8d9f049a0b8d0c26b5275de22
shaileshpandit141 <shaileshpandit141@gmail.com>
2024-05-30 17:00:53 +0530

    Define a handleCurrentUser function and add addEventListener for all block element in tic tac toe.

--- src/index.js
+++ src/index.js
@@ -7,3 +7,21 @@ const lossScoreEl = document.querySelector(".loss-score-number")
 let computer = "c"
 let user = "u"
 const scoreList = Array(9).fill(null)
+
+
+function handleCurrentUser(element, index) {
+    if (scoreList[index] === null) {
+        scoreList[index] = user
+        userStte.innerText = "computer"
+        element.innerText = user
+        chackUserWinner()
+        handleComputerClick()
+    }
+}
+
+
+blockElements.forEach((element, index) => {
+    element.addEventListener("click", (event) => {
+        handleCurrentUser(element, index)
+    })
+})
