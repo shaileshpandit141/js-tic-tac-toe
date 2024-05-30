a08d9800023964fed5c8551e8c1fb6d6128cf326
shaileshpandit141 <shaileshpandit141@gmail.com>
2024-05-30 16:58:40 +0530

    Define a dependency variables.

--- src/index.js
+++ src/index.js
@@ -2,3 +2,8 @@ const blockElements = document.querySelectorAll(".block")
 const userStte = document.querySelector(".current-user-state")
 const winnerScoreEl = document.querySelector(".winner-score-number")
 const lossScoreEl = document.querySelector(".loss-score-number")
+
+
+let computer = "c"
+let user = "u"
+const scoreList = Array(9).fill(null)
